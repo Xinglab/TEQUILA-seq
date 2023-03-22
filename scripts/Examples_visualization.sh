502f05efd8ad38679ceee05f5424b4569a1db709
Yang Xu <35879909+yuzhuxuyang@users.noreply.github.com>
2023-03-22 14:37:55 -0400

    Delete Examples_visualization.sh

--- scripts/Examples_visualization.sh
+++
@@ -1,5 +0,0 @@
-python ./Generate_bar_structure_figure_example.py --gene ENSG00000141510 --gene_name TP53 --transcript ESPRESSO:chr17:1864:802 --abundance_CPM_original ./Example_res/samples_N2_R0_abundance_CPM_target_genes.esp --name_col 4 --abundance_proportion ./Example_res/samples_N2_R0_abundance_proportion_target_genes_reshaped_merge_others.txt --abundance_CPM ./Example_res/samples_N2_R0_abundance_CPM_target_genes_reshaped_merge_others.txt --bedgraph ./Example_res/samples_BedGraph.bed --out_dir ./Example_res --order yes
-
-
-python ./Generate_bar_structure_figure_example.py --gene ENSG00000088305 --gene_name DNMT3B --transcript ENST00000348286 --abundance_CPM_original ./Example_res/samples_N2_R0_abundance_CPM_target_genes.esp --name_col 4 --abundance_proportion ./Example_res/samples_N2_R0_abundance_proportion_target_genes_reshaped_merge_others.txt --abundance_CPM ./Example_res/samples_N2_R0_abundance_CPM_target_genes_reshaped_merge_others.txt --bedgraph ./Example_res/samples_BedGraph.bed --out_dir ./Example_res --order yes
-
